0f623b3bf108ca3d988df1eb2b14cd6ce93fc932
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-06-10 14:36:08 -0400

    Checkbox fleshed out

--- Quiz/Quiz/Checkbox.cs
+++ Quiz/Quiz/Checkbox.cs
@@ -4,7 +4,39 @@ using System.Text;
 
 namespace Quiz
 {
-    public class Checkbox //: Question
+    public class Checkbox : Question
     {
+        public char[] CorrectAnswers { get; }
+
+        public Checkbox(string questionText, List<string> possibleSelections, char[] correctAnswers) : base(questionText, possibleSelections)
+        {
+            CorrectAnswers = correctAnswers;
+        }
+
+        public string GradeAnswer(char[] choices)
+        {
+            Array.Sort(CorrectAnswers);
+            Array.Sort(choices);
+            string answerKey = "";
+            string userInput = "";
+            foreach (char x in CorrectAnswers)
+            {
+                answerKey += x;
+            }
+
+            foreach (char x in choices)
+            {
+                userInput += x;
+            }
+
+            if (answerKey == userInput)
+            {
+                return "\nCorrect!";
+            }
+            else
+            {
+                return "\nSorry, that is incorrect.";
+            }
+        }
     }
 }
--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -7,12 +7,25 @@ namespace Quiz
     {
         static void Main(string[] args)
         {
-            //TESTING MULTIPLE CHOICE
             List<string> options = new List<string> { "Mitch", "Shea", "Victor", "Krombopulous Michael"};
-            MultipleChoice TestQuestion = new MultipleChoice("What is my name?", options, 'a');
-            Console.WriteLine(TestQuestion.ToString());
-            char choice = Console.ReadKey().KeyChar;
-            Console.WriteLine("\n" + TestQuestion.GradeAnswer(choice) + "\n");
+
+            //TESTING MULTIPLE CHOICE
+            //MultipleChoice TestQuestion = new MultipleChoice("What is my name?", options, 'a');
+            //Console.WriteLine(TestQuestion.ToString());
+            //char choice = Console.ReadKey().KeyChar;
+            //Console.WriteLine("\n" + TestQuestion.GradeAnswer(choice) + "\n");
+
+            //TESTING CHECKBOX
+            //Checkbox TestQuestion = new Checkbox("Who are my friends? (Choose 2)", options, new char[] { 'b', 'c' });
+            //Console.WriteLine(TestQuestion.ToString() + "\nFirst choice: ");
+            //char choice1 = Console.ReadKey().KeyChar;
+            //Console.WriteLine("\nSecond choice: ");
+            //char choice2 = Console.ReadKey().KeyChar;
+            //char[] choices = { choice1, choice2 };
+            //Console.WriteLine("\n" + TestQuestion.GradeAnswer(choices) + "\n");
+
+            //TESTING TRUEORFALSE
+
 
         }
     }
