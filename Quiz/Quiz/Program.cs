67d964dd4cf8e45fcba011f6ad15013b16d77526
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-06-10 13:04:46 -0400

    Rough framework

---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Quiz/Quiz/Question.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz
+{
+    class Question
+    {
+        public string QuestionText { get; set; }
+    }
+}
