5cf58cfdf7a164a80c30dc0f0ab0a8b83b08bb56
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-06-10 15:39:10 -0400

    Moved questions to a new quiz class

--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -8,29 +8,9 @@ namespace Quiz
     {
         static void Main(string[] args)
         {
-            List<string> options = new List<string> { "Mitch", "Shea", "Victor", "Krombopulous Michael"};
-
-            //TESTING MULTIPLE CHOICE
-            MultipleChoice McTestQuestion = new MultipleChoice("What is my name?", options, 'a');
-            Console.WriteLine(McTestQuestion.ToString());
-            char[] MCchoice = { Console.ReadKey().KeyChar };
-            Console.WriteLine("\n" + McTestQuestion.GradeAnswer(MCchoice) + "\n");
-
-            //TESTING CHECKBOX
-            Checkbox CheckboxTestQuestion = new Checkbox("Who are my friends? (Choose 2)", options, new char[] { 'b', 'c' });
-            Console.WriteLine(CheckboxTestQuestion.ToString() + "\nFirst choice: ");
-            char choice1 = Console.ReadKey().KeyChar;
-            Console.WriteLine("\nSecond choice: ");
-            char choice2 = Console.ReadKey().KeyChar;
-            char[] choices = { choice1, choice2 };
-            Console.WriteLine("\n" + CheckboxTestQuestion.GradeAnswer(choices) + "\n");
-
-            //TESTING TRUE OR FALSE
-            TrueOrFalse TFTestQuestion = new TrueOrFalse("In reality, I have no friends.", 'a');
-            Console.WriteLine(TFTestQuestion.ToString() + "\n");
-            char[] TFchoice = { Console.ReadKey().KeyChar };
-            Console.WriteLine("\n" + TFTestQuestion.GradeAnswer(TFchoice) + "\n");
-
+            Quiz.Question1();
+            Quiz.Question2();
+            Quiz.Question3();
         }
     }
 }
---
+++ Quiz/Quiz/Quiz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz
+{
+    class Quiz
+    {
+        private static List<string> options = new List<string> { "Mitch", "Shea", "Victor", "Krombopulous Michael" };
+
+        //TESTING MULTIPLE CHOICE
+        public static void Question1()
+        {
+            MultipleChoice McTestQuestion = new MultipleChoice("What is my name?", options, 'a');
+
+            Console.WriteLine(McTestQuestion.ToString());
+            char[] MCchoice = { Console.ReadKey().KeyChar };
+
+            Console.WriteLine("\n" + McTestQuestion.GradeAnswer(MCchoice) + "\n");
+        }
+
+        //TESTING CHECKBOX
+        public static void Question2()
+        {
+            Checkbox CheckboxTestQuestion = new Checkbox("Who are my friends? (Choose 2)", options, new char[] { 'b', 'c' });
+
+            Console.WriteLine(CheckboxTestQuestion.ToString() + "\nFirst choice: ");
+            char choice1 = Console.ReadKey().KeyChar;
+
+            Console.WriteLine("\nSecond choice: ");
+            char choice2 = Console.ReadKey().KeyChar;
+
+            char[] choices = { choice1, choice2 };
+            Console.WriteLine("\n" + CheckboxTestQuestion.GradeAnswer(choices) + "\n");
+        }
+
+        //TESTING TRUE OR FALSE
+        public static void Question3()
+        {
+            TrueOrFalse TFTestQuestion = new TrueOrFalse("In reality, I have no friends.", 'a');
+
+            Console.WriteLine(TFTestQuestion.ToString() + "\n");
+            char[] TFchoice = { Console.ReadKey().KeyChar };
+
+            Console.WriteLine("\n" + TFTestQuestion.GradeAnswer(TFchoice) + "\n");
+        }
+    }
+}
